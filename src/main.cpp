7b41547e3c8a7eb5ad3ffbaedc9399f9fc4b92aa
yossico <yossicohen19@gmail.com>
2017-09-23 19:16:20 +0300

    2ndtry-4

--- src/main.cpp
+++ src/main.cpp
@@ -34,9 +34,9 @@ int main()
 
   PID pid;
   // TODO: Initialize the pid variable.
-  double init_Kp = 0;
-  double init_Ki = 0;
-  double init_Kd = 0;
+  double init_Kp = -0.35;
+  double init_Ki = -0.1;
+  double init_Kd = -0.05;
   pid.Init(init_Kp, init_Ki, init_Kd);
 
   h.onMessage([&pid](uWS::WebSocket<uWS::SERVER> ws, char *data, size_t length, uWS::OpCode opCode) {
@@ -63,7 +63,7 @@ int main()
           */
           
           // DEBUG
-		  steer_value = pid.UpdateError();
+		  steer_value = pid.TotalError();
 
           std::cout << "CTE: " << cte << " Steering Value: " << steer_value << std::endl;
 
